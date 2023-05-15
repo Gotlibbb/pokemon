92376d0dc9996a4a71740e91fa7de791c0686393
Gleb <andryuhin.gleb@megapolis-it.ru>
2023-05-15 15:13:12 +0300

    redirect to /pokemons-list

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import {
+  useEffect
+} from 'react'
+import {
+  useRouter
+} from 'next/router'
+
+const Index = () => {
+  const router = useRouter()
+  useEffect(() => {
+    router.push('/pokemons-list')
+  }, [router])
+  return null
+}
+
+export default Index
